f98dc9268e97f43a7f8746b3f517f08d6550f851
dev4ult <nibrasbiasaaja@gmail.com>
2023-08-14 21:01:45 +0700

    feat: how to create a variable

---
+++ main.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	// without datatype in defining
+	var say = "Hello"
+	fmt.Println(say)
+
+	// define explicitly
+	var toName string = "John"
+	fmt.Println(toName)
+
+	// string concatenation
+	fmt.Println(say + " " + toName)
+
+	// multiple declare and define
+	var width, height int = 100, 70
+	fmt.Println("total area = ", width * height)
+
+	// declared without defined
+	var declared int
+	fmt.Println("without defined variable output : ", declared)
+
+	// short declaration
+	shortVar := "this is a short defined variable"
+	fmt.Println(shortVar)
+}
